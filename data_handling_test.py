b248fd4d398d6400be17a3b79a7283fd55c5456d
Karczel <92095164+Karczel@users.noreply.github.com>
2024-04-21 18:16:08 +0700

    Update data_handling_test.py

--- data_handling_test.py
+++ data_handling_test.py
@@ -47,7 +47,7 @@ for col in df.columns:
             nearest_before = find_nearest_previous_non_null(df[col], i)
             nearest_after = find_nearest_next_non_null(df[col], i)
             if pd.notnull(nearest_before) and pd.notnull(nearest_after):
-                df.loc[i,col] = (float(nearest_before) + float(nearest_after)) / 2
+                df.loc[i, col] = (float(nearest_before) + float(nearest_after)) / 2
 
 # ---convert dtypes of columns to correct type
 df['Time Serie'] = pd.to_datetime(df['Time Serie'])
@@ -55,8 +55,41 @@ for i in df.columns[1:]:
     df[i] = pd.to_numeric(df[i])
 
 # check
-print(df.isnull().sum())
+# print(df.isnull().sum())
 
 # ---Data Frame is ready for use (df)
 
 # ---rating of similarity
+def calc_slope(x):
+    slope = np.polyfit(range(len(x)), x, 1)[0]
+    return slope
+
+result = df[df.columns[1:]].rolling(31536000, min_periods=2).apply(calc_slope)
+
+def assign_rating(base_value, other_value):
+  base_sign = 1 if base_value >= 0 else -1
+  other_sign = 1 if other_value >= 0 else -1
+  diff = abs(base_value - other_value)
+
+  if base_sign == other_sign:
+      if diff < 0.0001:
+        return 2
+      elif diff < 0.01:
+        return 1
+  else:
+       if diff < 0.0001:
+        return -2
+       elif diff < 0.01:
+        return -1
+  return 0
+
+# Australian currency as base to compare to
+ratings_df = pd.DataFrame(index=df.index, columns=df.columns[1:])
+for idx, row in result.iterrows():
+    base_value = row['US$']
+    for col in result.columns[1:]:
+        other_value = row[col]
+        rating = assign_rating(base_value, other_value)
+        ratings_df.at[idx, col] = rating
+
+print(ratings_df)
